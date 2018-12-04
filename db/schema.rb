587e9c9faf482fb53699531c84906ea6f1480242
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2018-12-04 16:47:33 -0500

    revised the schema and re-ran migrations

--- db/schema.rb
+++ db/schema.rb
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2018_12_03_115042) do
     t.string "username"
     t.string "email"
     t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
