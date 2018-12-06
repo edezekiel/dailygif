df58ae20f131c594dc1ad4bbbce9110681f906a1
Kaeland Chatman <kaeland1@gmail.com>
2018-12-06 15:46:06 -0500

    show page routing errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,17 +6,16 @@ class SessionsController < ApplicationController
 
   def create
 
-    user = User.find_by(username: params[:user][:username])
+    @user = User.find_by(username: params[:user][:username])
 
-    user = user.try(:authenticate, params[:user][:password])
+    if @user.try(:authenticate, params[:user][:password])
+      session[:user_id] = @user.id
 
-    return redirect_to(controller: 'sessions', action: 'new') unless user
-
-    session[:user_id] = user.id
-
-    @user = user
-
-    redirect_to controller: 'welcome', action: 'profile' 
+      redirect_to controller: 'welcome', action: 'profile'
+    else
+      @user = User.new
+      render :new
+    end
   end
 
   def destroy
