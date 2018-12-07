694528f543cb01f7e997bbaa9670115aa3f705e1
Kaeland Chatman <kaeland1@gmail.com>
2018-12-07 15:00:01 -0500

    delete the password column

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :gifs
   has_secure_password
+  has_many :gifs
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -46,7 +46,6 @@ ActiveRecord::Schema.define(version: 2018_12_03_115042) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
